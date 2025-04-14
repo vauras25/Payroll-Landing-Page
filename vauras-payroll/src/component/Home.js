08cf50598769baf5330777495044fc7e7f3d8a4f
vauras25 <growth@vauras.in>
2025-04-14 14:52:43 +0530

    Update Home.js

--- vauras-payroll/src/component/Home.js
+++ vauras-payroll/src/component/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Dashboard from "./Dashboard";
-// import MetricsSection from "../component/MetricsSection/MetricsSection";
+import MetricsSection from "../component/MetricsSection/MetricsSection";
 import ComplianceSection from "./ComplianceSection";
 import FooterSection from "./FooterSection";
 import TimeTrackingSection from "./TimeTrackingSection";
 import Features from "./Features";
 import WallofLove from "./WallOfLove/WallOfLove"
-// import ValueComponent from "./ValueComponent/ValueComponent";
+ import ValueComponent from "./ValueComponent/ValueComponent";
 import CommunitySection from "./CommunitySection/CommunitySection"
 function Home() {
     return(
