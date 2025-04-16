129ab5d725dfa81ceacf88e175b0f7af9124bbf4
vauras25 <growth@vauras.in>
2025-04-16 14:02:47 +0530

    Add files via upload

--- vauras-payroll/src/component/Contact/Contact.js
+++ vauras-payroll/src/component/Contact/Contact.js
@@ -42,7 +42,18 @@ const Contact = () => {
           <p><FaPhoneAlt /> +91 98317766186</p>
           <p><FaEnvelope /> info@vauras.in</p>
           <div>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3685.9159816571437!2d88.35479882718931!3d22.5073352690767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0270da22bec5cd%3A0x9c9f5c7db304ee92!2sVauras%20Advisory%20Services%20Pvt.%20Ltd.%20%7C%20ESIC%20%26%20PF%20Consultant%20%7C%20Payroll%20Outsourcing%20%7C%20Labour%20Compliance%20%7C!5e0!3m2!1sen!2sin!4v1744787640644!5m2!1sen!2sin" width="600" height="450"  allowFullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3685.9159816571437!2d88.35479882718931!3d22.5073352690767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0270da22bec5cd%3A0x9c9f5c7db304ee92!2sVauras%20Advisory%20Services%20Pvt.%20Ltd.%20%7C%20ESIC%20%26%20PF%20Consultant%20%7C%20Payroll%20Outsourcing%20%7C%20Labour%20Compliance%20%7C!5e0!3m2!1sen!2sin!4v1744787640644!5m2!1sen!2sin" width="600" height="450"  allowFullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+
+            <iframe
+              title="Vauras Advisory Office Location"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3685.9159816571437!2d88.35479882718931!3d22.5073352690767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0270da22bec5cd%3A0x9c9f5c7db304ee92!2sVauras%20Advisory%20Services%20Pvt.%20Ltd.%20%7C%20ESIC%20%26%20PF%20Consultant%20%7C%20Payroll%20Outsourcing%20%7C%20Labour%20Compliance%20%7C!5e0!3m2!1sen!2sin!4v1744787640644!5m2!1sen!2sin"
+              width="600"
+              height="450"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+
           </div>
         </div>
       </div>
