b5fbde22a15895623a5c957fb92e1bf2a9e94f95
vauras25 <growth@vauras.in>
2025-04-16 15:08:24 +0530

    Add files via upload

--- vauras-payroll/src/component/Support/Chatbot/StartSlow.js
+++ vauras-payroll/src/component/Support/Chatbot/StartSlow.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Children } from "react";
 
 export default function StartBtn(props){
     const prefrance=(prefrance)=>{
--- vauras-payroll/src/component/Support/Support.js
+++ vauras-payroll/src/component/Support/Support.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../App.css'
 import Chatbot from 'react-chatbot-kit'
 import 'react-chatbot-kit/build/main.css'
