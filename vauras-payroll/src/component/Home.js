dd18cbf7895ee912cbde7b3f96475bdb158e43a1
vauras25 <growth@vauras.in>
2025-08-13 11:11:32 +0530

    Add files via upload

--- vauras-payroll/src/component/Home.js
+++ vauras-payroll/src/component/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Dashboard from "./Dashboard";
 import MetricsSection from "../component/MetricsSection/MetricsSection";
-import ComplianceSection from "./ComplianceSection";
-import FooterSection from "./FooterSection";
+// import ComplianceSection from "./ComplianceSection";
+// import FooterSection from "./FooterSection";
 import TimeTrackingSection from "./TimeTrackingSection";
-import Features from "./Features";
+// import Features from "./Features";
 import WallofLove from "./WallOfLove/WallOfLove"
 import ValueComponent from "./ValueComponent/ValueComponent";
-import CommunitySection from "./CommunitySection/CommunitySection"
+// import CommunitySection from "./CommunitySection/CommunitySection"
+import Practical_Features from "./Practical_Features/Practical_Features"
+import Reports from "./Reports/Reports"
+import Contact from "./Contact/Contact";
 function Home() {
     return(
 
@@ -15,12 +18,15 @@ function Home() {
         <Dashboard/>
        <MetricsSection/>
        <WallofLove/>
+       <Practical_Features/>
        <TimeTrackingSection/>
        <ValueComponent/>
-       <Features/>
-       <CommunitySection/>
+       <Reports/>
+       {/* <Features/> */}
+       {/* <CommunitySection/>
        <ComplianceSection/>
-       <FooterSection/>
+       <FooterSection/> */}
+       <Contact/>
         </div>
     );
   }
