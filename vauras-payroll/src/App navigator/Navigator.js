d47399fd2316fac26e00039a8f6522d56ee42f3d
vauras25 <growth@vauras.in>
2025-08-13 12:26:02 +0530

    Add files via upload

--- vauras-payroll/src/App navigator/Navigator.js
+++ vauras-payroll/src/App navigator/Navigator.js
@@ -24,42 +24,42 @@ const Navigator = () => {
       {/* Navigation Links */}
       <ul className={`nav-list ${menuOpen ? 'open' : ''}`}>
         <li className="nav-item">
-          <NavLink to="/" className="nav-link" end>
+          <NavLink to="/" className="nav-link1" end>
             Home
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/about" className="nav-link">
+          <NavLink to="/about" className="nav-link1">
             About
           </NavLink>
         </li>
         {/* <li className="nav-item">
-          <NavLink to="/Services" className="nav-link">
+          <NavLink to="/Services" className="nav-link1">
             Services
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/Career" className="nav-link">
+          <NavLink to="/Career" className="nav-link1">
             Career
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/support" className="nav-link">
+          <NavLink to="/support" className="nav-link1">
             Support
           </NavLink>
         </li> */}
         <li className="nav-item">
-          <NavLink to="/Contact" className="nav-link">
+          <NavLink to="/Contact" className="nav-link1">
             Contact
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink to="/Pricing" className="nav-link">
+          <NavLink to="/Pricing" className="nav-link1">
           Pricing
           </NavLink>
         </li>
         <li className="nav-item">
-        <NavLink to="/login" className="nav-link">
+        <NavLink to="https://vauras.cloud/login"  className="nav-link login-btn">
           Login
           </NavLink>
           {/* <a href="https://vauras.cloud/login" className="nav-link" target="_blank" rel="noopener noreferrer">
