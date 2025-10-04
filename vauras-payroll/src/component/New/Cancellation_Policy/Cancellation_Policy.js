1be365fc10ae8ddd51a6c3f77b953ae3b38cd71e
vauras25 <growth@vauras.in>
2025-10-04 15:48:23 +0530

    Add files via upload

---
+++ vauras-payroll/src/component/New/Cancellation_Policy/Cancellation_Policy.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './Cancellation_Policy.css'
+const CancellationPolicy = () => {
+  return (
+    <div className="cancellation_policy">
+      <h1>Cancellation Policy</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <p>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+        mollit anim id est laborum.
+      </p>
+      <p>
+        Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo 
+        pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris 
+        eu nibh euismod gravida.
+      </p>
+      <p>
+        Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo 
+        pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris 
+        eu nibh euismod gravida.
+      </p>
+      <p>
+        Donec mi odio, faucibus at, scelerisque quis, convallis in, nisi. Praesent nec nisl a purus 
+        blandit viverra. Praesent ut ligula non mi varius sagittis. Pellentesque habitant morbi 
+        tristique senectus et netus et malesuada fames ac turpis egestas.
+      </p>
+    </div>
+  );
+};
+
+export default CancellationPolicy;
