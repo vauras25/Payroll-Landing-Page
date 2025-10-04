a5dbf917f7a1a47e7408c2a95097a19aa63f264e
vauras25 <growth@vauras.in>
2025-10-04 15:49:21 +0530

    Add files via upload

--- vauras-payroll/src/Routes.js
+++ vauras-payroll/src/Routes.js
@@ -9,7 +9,11 @@ import Services from './component/Services/Services';
 import Career from './component/Career/Career';
 import Pricing  from './component/Priceing/Priceing';
 import Login from './component/login/login';
-
+import PrivacyPolicy from './component/New/Privacy_Policy/Privacy_Policy';
+import RefundPolicy from './component/New/Refund_Policy/RefundPolicy'
+import TermsUse from './component/New/Terms_Use/Terms_Use';
+import CancellationPolicy from './component/New/Cancellation_Policy/Cancellation_Policy'
+// import Pricing_New from './component/Pricing_New/Pricing_New'
 const AppRoutes = () => {
   return (
     <Routes>
@@ -20,6 +24,11 @@ const AppRoutes = () => {
        <Route path="/career" element={<Career />} /> 
        <Route path="/support" element={<Support />} /> 
        <Route path="/Pricing" element={<Pricing  />} /> 
+        <Route path="/privacy-policy" element={<PrivacyPolicy  />} /> 
+        <Route path="/refund-policy" element={<RefundPolicy  />} /> 
+        <Route path="/terms-of-use" element={<TermsUse  />} /> 
+        <Route path="/cancellation-policy" element={<CancellationPolicy  />} /> 
+       {/* <Route path="/Pricing_New" element={<Pricing_New  />} />  */}
        <Route path="/login" element={<Login/>} />
     </Routes>
   );
